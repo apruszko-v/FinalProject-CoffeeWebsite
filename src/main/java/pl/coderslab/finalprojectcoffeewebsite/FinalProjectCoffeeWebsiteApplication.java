15bae5814421998852da356685782c583b24e0c0
apruszko-v <apruszko@visa.com>
2025-08-29 13:11:46 +0200

    creating project

---
+++ src/main/java/pl/coderslab/finalprojectcoffeewebsite/FinalProjectCoffeeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.finalprojectcoffeewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectCoffeeWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectCoffeeWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/finalprojectcoffeewebsite/FinalProjectCoffeeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.finalprojectcoffeewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectCoffeeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
